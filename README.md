3da9ac2475443784f994e9bc855bf87d2771dac6
levinion <levinnion@gmail.com>
2026-01-24 22:35:28 +0800

    feat: add config support via vipera

--- README.md
+++ README.md
@@ -4,4 +4,36 @@ This is an implementation of [file-manager-interface](https://www.freedesktop.or
 
 It could be an alternative to [boydaihungst's implementation](https://github.com/boydaihungst/org.freedesktop.FileManager1.common).
 
-It only works with yazi for now, but I'd like to support custom commands then via a config file.
+## Configuration
+
+The application searches for the configuration file in the following order:
+
+1. `$XDG_CONFIG_HOME/filemanager1/config.toml`
+2. `~/.config/filemanager1/config.toml` 
+3. `/usr/share/filemanager1/config.toml`
+
+If no config file is found, the following default is used:
+
+```toml
+cmd = "foot -e yazi {}"
+```
+
+For example:
+
+As the service receives a call like:
+
+```shell
+gdbus call --session \
+  --dest org.freedesktop.FileManager1 \
+  --object-path /org/freedesktop/FileManager1 \
+  --method org.freedesktop.FileManager1.ShowFolders \
+  "['file://~/Documents', 'file://~/Downloads']" ""
+```
+
+The cmd will be transformed into:
+
+```bash
+foot -e yazi ~/Documents ~/Downloads
+```
+
+If there's more customized needs, try wrapping the command with a script instead.
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,52 @@
-use std::{error::Error, future::pending, path::PathBuf, process::Command};
+use serde::{Deserialize, Serialize};
+use std::{
+    error::Error, future::pending, os::unix::process::CommandExt, path::PathBuf, process::Command,
+};
+use vipera::{Configuration, Vipera};
 use zbus::{connection, interface};
 
-struct FileManager1 {}
+struct FileManager1 {
+    config: Config,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Config {
+    cmd: String,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            cmd: "foot -e yazi {}".to_owned(),
+        }
+    }
+}
+
+impl Configuration for Config {
+    fn vipera() -> vipera::Vipera {
+        Vipera::new()
+            .set_config_name("config.toml")
+            .add_config_path("$XDG_CONFIG_HOME/filemanager1")
+            .unwrap()
+            .add_config_path("~/.config/filemanager1")
+            .unwrap()
+            .add_config_path("/usr/share/filemanager1")
+            .unwrap()
+    }
+}
 
 #[interface(name = "org.freedesktop.FileManager1")]
 impl FileManager1 {
     fn show_folders(&mut self, uris: Vec<String>, _startup_id: String) {
-        let dirs = uris
+        let items = uris
             .iter()
             .filter_map(|uri| uri.strip_prefix("file://"))
             .filter(|path| PathBuf::from(path).is_dir())
             .collect::<Vec<_>>()
             .join(" ");
-        let items = format!("setsid foot -e yazi {}", dirs);
+        let items = self.config.cmd.replace("{}", &items);
         let args = vec!["-c", &items];
-        let _ = Command::new("sh").args(args).spawn();
+        let _ = Command::new("sh").args(args).process_group(0).spawn();
     }
 
     fn show_items(&mut self, uris: Vec<String>, _startup_id: String) {
@@ -24,9 +56,9 @@ impl FileManager1 {
             .filter(|path| PathBuf::from(path).is_file())
             .collect::<Vec<_>>()
             .join(" ");
-        let items = format!("setsid foot -e yazi {}", items);
+        let items = self.config.cmd.replace("{}", &items);
         let args = vec!["-c", &items];
-        let _ = Command::new("sh").args(args).spawn();
+        let _ = Command::new("sh").args(args).process_group(0).spawn();
     }
 
     fn show_item_properties(&mut self, uris: Vec<String>, _startup_id: String) {
@@ -35,15 +67,16 @@ impl FileManager1 {
             .filter_map(|uri| uri.strip_prefix("file://"))
             .collect::<Vec<_>>()
             .join(" ");
-        let items = format!("setsid foot -e yazi {}", items);
+        let items = self.config.cmd.replace("{}", &items);
         let args = vec!["-c", &items];
-        let _ = Command::new("sh").args(args).spawn();
+        let _ = Command::new("sh").args(args).process_group(0).spawn();
     }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let filemanager1 = FileManager1 {};
+    let config = Config::read_in_config().unwrap_or_default();
+    let filemanager1 = FileManager1 { config };
     let _conn = connection::Builder::session()?
         .name("org.freedesktop.FileManager1")?
         .serve_at("/org/freedesktop/FileManager1", filemanager1)?
